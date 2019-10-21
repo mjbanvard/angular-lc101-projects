6c6bd2bce263987e87d8d21c6204954e6ecfaed7
Michael Banvard <mjbanvard@gmail.com>
2019-10-21 11:15:55 -0500

    First save and commit.

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'A small selection of my Favorite Images';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/2152/s300/launchcode-20logo.jpeg';
+  image3 = 'https://www.astrology-zodiac-signs.com/images/Scorpio-w.png';
 
   constructor() { }
 
